74166be40fa25072704026bb53035b7733a5965c
sky0-0glitch <157227184+sky0-0glitch@users.noreply.github.com>
2026-06-05 15:47:53 -0400

    bug 5 replaced the statement to preparedStatement to prevent injection

--- src/main/java/com/mangomusic/data/AlbumDao.java
+++ src/main/java/com/mangomusic/data/AlbumDao.java
@@ -58,27 +58,26 @@ public class AlbumDao {
         String query = "SELECT al.album_id, al.artist_id, al.title, al.release_year, ar.name as artist_name " +
                 "FROM albums al " +
                 "JOIN artists ar ON al.artist_id = ar.artist_id " +
-                "WHERE ar.primary_genre = '" + genre + "' " +
+                "WHERE ar.primary_genre = ?" +
                 "ORDER BY al.title";
-
-        try {
+        //bug 5 used preparedStatement instead of statement to defend against injection
+        try (
             Connection connection = dataManager.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setString(1,genre);
+                try (ResultSet results = statement.executeQuery()) {
+                    while (results.next()) {
+                        int albumId = results.getInt("album_id");
+                        int artistId = results.getInt("artist_id");
+                        String title = results.getString("title");
+                        int releaseYear = results.getInt("release_year");
+                        String artistName = results.getString("artist_name");
 
-            try (Statement statement = connection.createStatement();
-                 ResultSet results = statement.executeQuery(query)) {
-
-                while (results.next()) {
-                    int albumId = results.getInt("album_id");
-                    int artistId = results.getInt("artist_id");
-                    String title = results.getString("title");
-                    int releaseYear = results.getInt("release_year");
-                    String artistName = results.getString("artist_name");
-
-                    albums.add(new Album(albumId, artistId, title, releaseYear, artistName));
+                        albums.add(new Album(albumId, artistId, title, releaseYear, artistName));
+                    }
                 }
-            }
 
-        } catch (SQLException e) {
+            } catch (SQLException e) {
             System.err.println("Error getting albums by genre: " + e.getMessage());
             e.printStackTrace();
         }
